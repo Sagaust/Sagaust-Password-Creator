6edf630785a992c0107bd6cf792f194a66a3d27d
Sagaust <austinorge@yahoo.com>
2023-01-10 23:14:03 +0000

    all functions successfully tested and the app works well

--- script.js
+++ script.js
@@ -98,12 +98,19 @@ let userChoice = [];
 function getPasswordOptions() {
 // a prompt for password length:
 // *the user inputs the length between 10 and 65
-passwordLength = parseInt(prompt('How many characters would you like your password to contain?'));
-if (passwordLength < 8 || passwordLength > 65) {
-  alert('Password must be in between 10 and 65 characters long. Please try again');
-  choiceValidation = false;
-  return;
- } else {
+    passwordLength = parseInt(prompt('How many characters would you like your password to contain?'));
+    if(isNaN(passwordLength)) {
+        alert("Kindly type in a number.")
+        confirm();
+        choiceValidation = false;
+        return;
+    } 
+    else if(passwordLength < 10  || passwordLength > 65) {
+        alert('Password must be in between 10 and 65 characters long. Please try again');
+        choiceValidation = false;
+        return;
+    }
+    else {
         // a prompt for special character:
         // *the user clicks 'ok' or 'cancel', which returns 'true' or 'false'
         var includeSpecialCharacters = confirm('Click OK to confirm including special characters');
@@ -113,47 +120,78 @@ if (passwordLength < 8 || passwordLength > 65) {
         var includeLowerCaseCharacters = confirm('Click OK to confirm including lowercase characters');
         // a prompt for uppercase character:
         var includeUppercaseCharacters = confirm('Click OK to confirm including uppercase characters');
- }
 
+        userChoice = [includeSpecialCharacters, includeLowerCaseCharacters, includeUppercaseCharacters, includeNumericCharacters];
 
-
+        if(userChoice.every(i => i === false)) {
+            confirm("Kindly make a choice of at least one character type.");
+            return;
+        } else {
+            console.log("Valid inputs");
+            choiceValidation = true;
+        }
+    }
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-    return arr(Math.floor(Math.random() * arr.length))
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function characterSetSelector() {
+    if(userChoice[i] === true){
+        console.log(getRandom(userChoice[i]));
+    }
 }
 
 // Function to generate password with user input
 function generatePassword() {
     getPasswordOptions();
-    if(validPass === true){
-        let i = 0;
-        temporaryPassword = []
+    if(choiceValidation === true){
+        var i = 0;
+        temporaryPassword = [];
+        while (i < passwordLength){
+            if(userChoice[0] === true) {
+                temporaryPassword.push(getRandom(specialCharacters));
+                i++;
+            }
+            if(userChoice[1] === true) {
+                temporaryPassword.push(getRandom(lowerCasedCharacters));
+                i++;
+            }
+            if(userChoice[2] === true) {
+                temporaryPassword.push(getRandom(upperCasedCharacters));
+                i++;
+            }
+            if(userChoice[3] === true) {
+                temporaryPassword.push(getRandom(numericCharacters));
+                i++;
+            }
+        }
+        password = temporaryPassword.join("");
     }
-    if (includeSpecialCharacters.checked) {
-        password += getRandom(upperCasedCharacters)
-    }
-    if (includeLowercaseCharacters.checked) {
-        password += getRandom(lowerCasedCharacters)
-    }
-    if (includeSpecialCharacters.checked) {
-        password += getRandom(specialCharacters)
-    }
-    if (includeNumericCharacters.checked) {
-        password += getRandom(numericCharacters)
+    else{
+        return;
     }
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
+function resetPassword(){
+    let password = "Unable to generate password"; 
+    let passwordLength = 0;
+    let choiceValidation = false;
+    let userChoice = [];
+}
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    generatePassword();
     var passwordText = document.querySelector('#password');
 
     passwordText.value = password;
+    resetPassword();
 }
 
 // Add event listener to generate button
